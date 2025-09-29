c5f4dc9aecb2072b80a7cc9c4586cfc79432161b
Mohamed Zakaria <mz7891555@gmail.com>
2025-09-29 13:29:43 +0300

    Update footer

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -2,14 +2,16 @@ import React from 'react';
 import '../../styles/footer.css'
 import rubyImg  from '../../images/RubyCosmo.png'
 const Footer = () => {
+      const year = new Date().getFullYear();
+
     return (
         <div className='footer'>
             <div className="ruby-parent">
             <img src={rubyImg} alt="ruby-logo" className='ruby-logo'/>
             <h4>RubyCosmo</h4>
             </div>
-            <p>By:M.Zakaria</p>
-            <p>Copyrights ©️ 2022 </p>
+            <p>By: M.Zakaria</p>
+            <p>Copyrights ©️ {year} </p>
         </div>
     );
 }
